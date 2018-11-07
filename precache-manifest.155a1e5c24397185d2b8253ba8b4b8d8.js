4614fef2612a7b97f149fbe815f6b6d70d2a441e
Hopp Willi <38337505+jhwillia7@users.noreply.github.com>
2018-11-06 22:51:38 -0800

    Updates

---
+++ precache-manifest.155a1e5c24397185d2b8253ba8b4b8d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "575b1696562213af3dec3c7b8d0721e8",
+    "url": "/clickeygamegitpages/static/media/warriors.575b1696.png"
+  },
+  {
+    "revision": "95ae9cf06f2061a322512a6d3f7860fe",
+    "url": "/clickeygamegitpages/static/media/nbalogo.95ae9cf0.png"
+  },
+  {
+    "revision": "c05f223e61662eeb568335c8c60d1865",
+    "url": "/clickeygamegitpages/static/media/clickylogo.c05f223e.PNG"
+  },
+  {
+    "revision": "48fe4d02541296b3cf96",
+    "url": "/clickeygamegitpages/static/js/runtime~main.48fe4d02.js"
+  },
+  {
+    "revision": "2d365d9dc78ba91ab82d",
+    "url": "/clickeygamegitpages/static/js/main.2d365d9d.chunk.js"
+  },
+  {
+    "revision": "817fc05f03fb18daf801",
+    "url": "/clickeygamegitpages/static/js/1.817fc05f.chunk.js"
+  },
+  {
+    "revision": "2d365d9dc78ba91ab82d",
+    "url": "/clickeygamegitpages/static/css/main.77115945.chunk.css"
+  },
+  {
+    "revision": "d7d010bbd7fcb349aac79bfb51e34824",
+    "url": "/clickeygamegitpages/index.html"
+  }
+];
